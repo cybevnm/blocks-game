84e1e16ab8a7f6b57218245a3934348854c36ef4
cybevnm <cybevnm@gmail.com>
2013-04-28 00:45:04 +0300

    Unnecessary files deleted

--- load.lisp
+++
@@ -1,8 +0,0 @@
-(pushnew "/home/cybevnm/proj/lisp/lispbuilder/lispbuilder-sdl/" asdf:*central-registry* :test #'equal)
-(pushnew "/home/cybevnm/proj/lisp/lispbuilder/lispbuilder-sdl-mixer/" asdf:*central-registry* :test #'equal)
-
-(asdf:oos 'asdf:load-op 'lispbuilder-sdl)
-(asdf:oos 'asdf:load-op 'lispbuilder-sdl-examples)
-
-(load "packages.lisp")
-(load "blocks.lisp")
--- packages.lisp
+++
@@ -1,4 +0,0 @@
-(in-package :cl-user)
-
-(defpackage :blocks-game
-  (:use :common-lisp))
